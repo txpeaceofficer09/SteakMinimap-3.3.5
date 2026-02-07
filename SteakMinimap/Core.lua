8383d186580b81fc50f6a9231b54a264bc4f6ef2
txpeaceofficer09 <james.mccaughey@gmail.com>
2026-02-07 07:24:45 -0600

    Updates to add quest tracking.

--- SteakMinimap/Core.lua
+++ SteakMinimap/Core.lua
@@ -23,8 +23,9 @@ f:SetPoint("BOTTOMRIGHT", UIParent, "BOTTOMRIGHT", 0, 20)
 f:SetSize(190, 150)
 f:SetBackdrop( { bgFile = "Interface\\DialogFrame\\UI-DialogBox-BackGround-Dark", edgeFile = nil, tile = true, tileSize = 32, edgeSize = 0, insets = { left = 0, right = 0, top = 0, bottom = 0 } } )
 
-f:SetFrameStrata("TOOLTIP")
-f:SetFrameLevel(MainMenuBar:GetFrameLevel()+10)
+f:SetFrameStrata("HIGH")
+--f:SetFrameLevel(MainMenuBar:GetFrameLevel()+10)
+f:SetFrameLevel(25)
 
 local sc = CreateFrame("Frame", "MapFrameSC", MapFrame)
 
@@ -51,11 +52,64 @@ sc:Show()
 
 f:Show()
 
+local blobColors = {
+    {r=255, g=0,   b=0,   a=100}, -- 1: Red
+    {r=0,   g=255, b=0,   a=100}, -- 2: Neon Green
+    {r=0,   g=0,   b=255, a=100}, -- 3: Blue
+    {r=255, g=255, b=0,   a=100}, -- 4: Yellow
+    {r=255, g=0,   b=255, a=100}, -- 5: Magenta
+    {r=0,   g=255, b=255, a=100}, -- 6: Cyan
+    {r=255, g=128, b=0,   a=100}, -- 7: Orange
+    {r=128, g=0,   b=255, a=100}, -- 8: Deep Purple
+    {r=0,   g=128, b=0,   a=100}, -- 9: Dark Green
+    {r=255, g=153, b=204, a=100}, -- 10: Hot Pink
+    {r=153, g=255, b=153, a=100}, -- 11: Pale Green
+    {r=102, g=102, b=102, a=100}, -- 12: Grey
+    {r=255, g=255, b=255, a=100}, -- 13: White
+    {r=102, g=51,  b=0,   a=100}, -- 14: Brown
+    {r=0,   g=102, b=102, a=100}, -- 15: Teal
+    {r=255, g=204, b=0,   a=100}, -- 16: Gold
+    {r=204, g=255, b=0,   a=100}, -- 17: Lime
+    {r=0,   g=204, b=153, a=100}, -- 18: Mint
+    {r=0,   g=0,   b=128, a=100}, -- 19: Navy
+    {r=255, g=102, b=102, a=100}, -- 20: Salmon
+    {r=153, g=0,   b=0,   a=100}, -- 21: Maroon
+    {r=204, g=153, b=255, a=100}, -- 22: Lavender
+    {r=255, g=255, b=153, a=100}, -- 23: Cream
+    {r=0,   g=51,  b=102, a=100}, -- 24: Midnight Blue
+    {r=128, g=128, b=0,   a=100}  -- 25: Olive
+}
+
+local questBlobs = {}
+local function CreateBlob(index, questID)
+	if not questBlobs[index] then
+		local BlobFrame = CreateFrame("QuestPOIFrame", "MapBlobFrame_"..index, MapFrameSC)
+
+		BlobFrame:SetPoint("TOPLEFT", MapFrameSC, "TOPLEFT", 0, 0)
+		BlobFrame:SetSize(1002, 662)
+		BlobFrame:Show()
+
+		BlobFrame:SetFillTexture("Interface\\WorldMap\\UI-QuestBlob-Inside")
+		BlobFrame:SetBorderTexture("Interface\\WorldMap\\UI-QuestBlob-Outside")
+
+		BlobFrame:SetFillAlpha(128)
+		BlobFrame:SetBorderAlpha(255)
+		BlobFrame:SetBorderScalar(1.0) -- Thickness of the border
+
+		--BlobFrame:SetFillColor(blobColors[index].r, blobColors[index].g, blobColors[index].b, blobColors[index].a)
+		--BlobFrame:SetBorderColor(blobColors[index].r, blobColors[index].g, blobColors[index].b, blobColors[index].a+100)
+
+		questBlobs[index] = BlobFrame
+	end
+
+	return questBlobs[index]
+end
+
 --local PlayerArrow = CreateFrame("Frame", "MapFramePlayerArrowFrame", MapFrame)
 local PlayerArrow = CreateFrame("Frame", "MapFramePlayerArrowFrame", MapFrameSC)
 PlayerArrow:SetSize(64, 64)
 --PlayerArrow:SetPoint("CENTER", MapFrame, "CENTER", 0, 0)
-PlayerArrow:SetFrameLevel(MapFrameSC:GetFrameLevel() + 10)
+PlayerArrow:SetFrameLevel(MapFrameSC:GetFrameLevel() + 25)
 
 PlayerArrow.texture = PlayerArrow:CreateTexture(nil, "OVERLAY")		
 PlayerArrow.texture:SetAllPoints(PlayerArrow)
@@ -76,6 +130,26 @@ local function IsInCity()
     end
 
     return false 
+end
+
+local function MapFrame_UpdateBlobs()
+    --MapBlobFrame:Update() -- Internal engine refresh
+    for _, blob in ipairs(questBlobs) do blob:Hide() end
+
+    for i = 1, GetNumQuestLogEntries() do
+        --local _, _, _, _, _, isHeader, _, _, questID = GetQuestLogTitle(i)
+        local questTitle, level, questTag, suggestedGroup, isHeader, isCollapsed, isComplete, isDaily, questID = GetQuestLogTitle(i)
+        if not isHeader and questID then
+            -- Draw the blob regardless of whether it has a specific POI icon
+
+            --MapBlobFrame:DrawQuestBlob(questID, true)
+
+            local BlobFrame = questBlobs[i] or CreateBlob(i)
+
+            --BlobFrame:DrawQuestBlob(questID, isComplete)
+            BlobFrame:DrawQuestBlob(questID, not isComplete)
+        end
+    end
 end
 
 local UnitDots = {}
@@ -117,6 +191,74 @@ local function CreatePOI(index)
 	end
 
 	return poiDots[index]
+end
+
+local function MapFrame_UpdateQuestIcons()
+    local mapW = MapFrameSC:GetWidth()
+    local mapH = MapFrameSC:GetHeight()
+
+    for _, child in ipairs({MapFrameSC:GetChildren()}) do
+    	if strsub(child:GetName(), 1, 15) == "MapFrameSC_POI_" then child:Hide() end
+    end
+
+    for i = 1, GetNumQuestLogEntries() do
+        --local questTitle, level, questTag, suggestedGroup, isHeader, isCollapsed, isComplete, isDaily, questID = GetQuestLogTitle(i)
+        local _, _, _, _, isHeader, _, isComplete, _, questID = GetQuestLogTitle(i)
+        
+        if not isHeader then
+            local _, posX, posY, objective = QuestPOIGetIconInfo(questID)
+
+            if posX and posX > 0 then
+                --local name = "MapFrameSC_POI_" .. questID
+                local name = "MapFrameSC_POI_"
+                local btn = _G[name]
+
+                if isComplete then
+                	--name = name.."Complete_"..i
+                	name = name.."Complete_"..questID
+                else
+                	--name = name.."Number_"..i
+                	name = name.."Number_"..questID
+                end
+
+                if not btn then
+					if ( isComplete ) then
+                    	    btn = CreateFrame("Button", name, MapFrameSC, "QuestPOICompletedTemplate");
+                	else
+                        	btn = CreateFrame("Button", name, MapFrameSC, "QuestPOITemplate");
+                	end
+
+                	--btn:SetScript("OnEnter", function(self) self.grow = true; end)
+					--btn:SetScript("OnLeave", function(self) self.grow = false; end)
+					btn:SetScript("OnEnter", nil)
+					btn:SetScript("OnLeave", nil)
+					btn:SetScript("OnClick", nil)
+					btn:EnableMouse(false)
+                end
+
+                local finalX = posX * mapW
+                local finalY = -posY * mapH
+
+                btn:SetParent(MapFrameSC)
+                btn:ClearAllPoints()
+                btn:SetPoint("CENTER", MapFrameSC, "TOPLEFT", finalX, finalY)
+                btn:SetFrameLevel(MapFrameSC:GetFrameLevel() + 5)
+
+                -- 6. Set the Icon (Numeric or Question Mark)
+                --if isComplete then
+                --    QuestPOI_SelectButtonType(btn, QUEST_POI_COMPLETE_SWAP, i)
+                --else
+                --    QuestPOI_SelectButtonType(btn, QUEST_POI_NUMERIC, i)
+                --end
+
+				local yOffset = 0.5 + floor(i / QUEST_POI_ICONS_PER_ROW) * QUEST_POI_ICON_SIZE;
+				local xOffset = mod(i, QUEST_POI_ICONS_PER_ROW) * QUEST_POI_ICON_SIZE;
+				if btn.number then btn.number:SetTexCoord(xOffset, xOffset + QUEST_POI_ICON_SIZE, yOffset, yOffset + QUEST_POI_ICON_SIZE); end
+
+                btn:Show()
+            end
+        end
+    end
 end
 
 function MoveMinimapButtons()
@@ -285,13 +427,20 @@ local function OnEvent(self, event, ...)
 		MoveMinimapButtons()
 		Minimap:SetMaskTexture("Interface\\Buttons\\WHITE8X8")
 		self:Show()
-		--UpdateMyMapPOIs()
+
+		MapFrame_UpdateQuestIcons()
+		MapFrame_UpdateBlobs()
 	elseif event == "WORLD_MAP_UPDATE" or event == "ZONE_CHANGED" or event == "ZONE_CHANGED_INDOORS" or event == "WORLD_MAP_NAME_UPDATE" then
 		MapFrame_UpdateTextures()
+		MapFrame_UpdateQuestIcons()
+		MapFrame_UpdateBlobs()
 	elseif event == "ZONE_CHANGED_NEW_AREA" then
 		MapFrame_UpdateTextures()
+		MapFrame_UpdateQuestIcons()
+		MapFrame_UpdateBlobs()
 	elseif event == "QUEST_LOG_UPDATE" or event == "QUEST_POI_UPDATE" then
-		--UpdateMyMapPOIs()
+		MapFrame_UpdateQuestIcons()
+		MapFrame_UpdateBlobs()
 	end	
 end
 
@@ -414,63 +563,61 @@ f:SetScript("OnUpdate", function(self, elapsed)
     				dot:Show()
     			end
     		end
-	end
+		end
 
-	self.dotTimer = 0
+		self.dotTimer = 0
 
-	self.growTimer = (self.growTimer or 0) + elapsed
+		--self.growTimer = (self.growTimer or 0) + elapsed
 
-	--if self.growTimer >= 0.01 then
+		--if self.growTimer >= 0.01 then
     		local width = MapFrame:GetWidth()
     		local height = MapFrame:GetHeight()
 
-    		local scaleX = (500-190)/3
-    		local scaleY = (500-150)/3
+	    	local scaleX = (500-190)/2
+    		local scaleY = (500-150)/2
 
     		if (self.grow or false) == true then
-    			if width < 500 then width = width + scaleX end
+	    		if width < 500 then width = width + scaleX end
     			if width > 500 then width = 500 end
 
-    			if height < 500 then height = height + scaleY end
-    			if height > 500 then height = 500 end
-    		else
-    			if width > 190 then width = width - scaleX end
-    			if width < 190 then width = 190 end
+   				if height < 500 then height = height + scaleY end
+   				if height > 500 then height = 500 end
+   			else
+   				if width > 190 then width = width - scaleX end
+   				if width < 190 then width = 190 end
 
-    			if height > 150 then height = height - scaleY end
-    			if height < 150 then height = 150 end
-    		end
+   				if height > 150 then height = height - scaleY end
+   				if height < 150 then height = 150 end
+	   		end
 
-   			MapFrame:SetSize(width, height)
+			MapFrame:SetSize(width, height)
 
-    		--self.growTimer = 0
-    	--end
+	   		--self.growTimer = 0
+	   	--end
+
 	end
 end)
 
-MapFrame:SetScript("OnEnter", function(self)
-	self.grow = true
-end)
+local growFrames = { MapFrame, Minimap, MapFrameSC }
+for _, frame in ipairs(growFrames) do
+	frame:SetScript("OnEnter", function(self) MapFrame.grow = true end)
+	frame:SetScript("OnLeave", function(self) MapFrame.grow = false end)
+end
 
-MapFrame:SetScript("OnLeave", function(self)
-	self.grow = false
-end)
+--MapFrame:SetScript("OnEnter", function(self) self.grow = true; end)
+--MapFrame:SetScript("OnLeave", function(self) self.grow = false; end)
 
-Minimap:SetScript("OnEnter", function(self)
-	MapFrame.grow = true
-end)
+--Minimap:SetScript("OnEnter", function(self) MapFrame.grow = true; end)
+--Minimap:SetScript("OnLeave", function(self) MapFrame.grow = false; end)
 
-Minimap:SetScript("OnLeave", function(self)
-	MapFrame.grow = false
-end)
+--MapFrameSC:SetScript("OnEnter", function(self) MapFrame.grow = true; end)
+--MapFrameSC:SetScript("OnLeave", function(self) MapFrame.grow = false; end)
 
-MapFrameSC:SetScript("OnEnter", function(self)
-	MapFrame.grow = true
-end)
+--MapFrameSC:SetScript("OnEnter", function(self) MapFrame:SetSize(500, 500) end)
+--MapFrameSC:SetScript("OnLeave", function(self) MapFrame:SetSize(190, 150) end)
 
-MapFrameSC:SetScript("OnLeave", function(self)
-	MapFrame.grow = false
-end)
+--MapFrame:EnableMouse(false)
+--Minimap:EnableMouse(false)
 
 f:SetScript("OnMouseWheel", function(self, delta)
 	if delta > 0 and MapFrameSC:GetScale() < 1 then
@@ -542,3 +689,5 @@ SlashCmdList["MAPSCALE"] = function(msg)
     	print("Map scale: "..MAP_SCALE)
     end
 end
+
+--GameTooltip:SetFrameLevel(MapFrame:GetFrameLevel()+100)
